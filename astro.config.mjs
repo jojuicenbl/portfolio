686f2d10648f5d04e985e7ebbbc9dd1366ef0a72
jojuicenbl <joris.neblai@icloud.com>
2025-11-04 22:02:07 +0100

    set up netlify adapter to online deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: netlify(),
 });
