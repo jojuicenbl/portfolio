d9af8d5ec61e6c790dcd9ddc4750c5acbce73387
jojuicenbl <joris.neblai@icloud.com>
2025-11-07 12:43:01 +0100

    core: SEO improvements (sitemap, metadata, robots.txt)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from '@astrojs/tailwind';
 
 import node from '@astrojs/node';
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://jorisneblai.xyz',
+  integrations: [tailwind(), sitemap()],
   output: 'server',
   adapter: netlify(),
 });
